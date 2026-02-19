c776715b6162bb9839b00b54a171d051fb02de55
Ammu <mallavarapuasritha@gmail.com>
2026-02-19 12:38:25 +0530

    Initial commit

---
+++ MakeMyTripProject/src/main/java/com/example/demo/exceptions/FlightTicketCancel.java
@@ -0,0 +1,18 @@
+package com.example.demo.exceptions;
+
+public class FlightTicketCancel extends RuntimeException{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FlightTicketCancel(String msg) {
+
+		super(msg);
+		
+	}
+	
+	
+
+}
